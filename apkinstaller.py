33ea3b2d69918751b6df80c73f7bea1f60183a51
aaxyat <aaxyat@gmail.com>
2020-08-05 23:34:25 +0545

    Added a botched windows support

--- apkinstaller.py
+++ apkinstaller.py
@@ -1,8 +1,17 @@
 from os import listdir, chdir
 import subprocess
 from os.path import isfile, join
-onlyfiles = [f for f in listdir(
-    "/home/aaxyat/Downloads/Apks") if isfile(join("/home/aaxyat/Downloads/Apks", f))]
-chdir("/home/aaxyat/Downloads/Apks")
-for i in onlyfiles:
-    subprocess.call(f"adb install {i}", shell=True)
+
+
+def apk_path(path):
+    apks = [f for f in listdir(
+        path) if isfile(join(path, f))]
+    return apks
+
+
+path = input("please Enter the full path where your apk is located: ")
+chdir(path)
+
+
+for items in apk_path(path):
+    subprocess.call(f"adb install {items}", shell=True)
